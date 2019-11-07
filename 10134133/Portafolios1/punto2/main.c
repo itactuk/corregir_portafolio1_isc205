9aca0d1ae50b83ad0a40a18d8cefa85b3743f883
nicoluv <55165208+nicoluv@users.noreply.github.com>
2019-11-07 13:18:43 -0400

    -nicol ureña

--- 10134133/Portafolios1/punto2/main.c
+++ 10134133/Portafolios1/punto2/main.c
@@ -2,21 +2,24 @@
 #include <stdarg.h>
 
 
-double average (int cnt,...);
+double average (double cnt,...);//-10 ES DE doubles
 
 int main() {
+
     printf("Maximo!\n");
-    printf( "Average Maximo: %lf\n",average(3,1.5,4.8,2.5));
+    printf( "Average Maximo: %.2f\n",average(3,1.554,4.8345,2.5));
     return 0;
 }
 
-double average (int cnt,...) {
-    double max = 0, i;
+double average (double cnt,...) {
+
+    double max = 0, i,n;
     va_list x;
+
     va_start(x, cnt);
     for (i = 0; i < cnt; i++)
     {
-        int n = va_arg(x, double);
+        n = va_arg(x, double);//-5 declarar n en el ciclo le quita el valor de los decimale
         if (n > max) {
             max = n;
         }
