64812c0e5b08b604e0a411a614f162f4f9276966
nicoluv <55165208+nicoluv@users.noreply.github.com>
2019-11-07 09:43:40 -0400

    -nicol ureña

--- 10135663/I_guess/7.c
+++ 10135663/I_guess/7.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 
-int Sumar_Matriz(int i, int j,int x, int matriz[i][j], int suma)
+int Sumar_Matriz(int f, int c, int matriz[f][c], int suma)
 {
 
-    if(i < 0)
+
+//    if(i < 0)//las condiciones no tienen sentido, no retorna la suma de la matriz, mejor emplea un bucle.-20
+//    {
+//        return suma;
+//    }
+//    if(i >= 0)
+//    {
+//        if(j>=0)
+//        {
+//            suma += matriz[i][j];
+//            Sumar_Matriz(i,j-1,matriz,suma);
+//        }
+//        if(j < 0)
+//        {
+//            Sumar_Matriz(i-1,j=2,matriz,suma);
+//        }
+//
+//    }
+
+    for(int i=0;i< f;i++)
     {
-        return suma;
-    }
-    if(i >= 0)
-    {
-        if(j>=0)
+        for(int j=0;j< c;j++)
         {
-            suma += matriz[i][j];
-            Sumar_Matriz(i,j-1,x,matriz,suma);
-        }
-        if(j < 0)
-        {
-            Sumar_Matriz(i-1,j=x,x,matriz,suma);
+            suma +=	matriz[i][j];
+
         }
 
     }
+    return  suma;
+
 }
 
 int main(void){
-    int i=2,j=2,x=j,suma=0;
-    int matriz[3][3]={{1,1,1},
+
+    int f=3,c=3,suma=0, sumar;// x se puede ahorrar perfectamente -5  //llamando a f commo cant de filas y c como cant de columnas
+
+    int matriz[3][3]={{2,1,1},//cambie el primer uno por un dos,en el original y dio 12 en lugar de 10, ahora da 10 como deberia
                       {1,1,1},
                       {1,1,1}};
-    suma = Sumar_Matriz(i,j,x,matriz,suma);
-    printf("La suma de las cantidades en la matriz es: %d",suma);
+    sumar = Sumar_Matriz(f,c,matriz,suma);//-5 por usar suma como parametro, pero tambien usarla para llamar a la funcion, debiste declarar otra.
+    printf("La suma de las cantidades en la matriz es: %d",sumar);
 }
