ec61f14c815ac85b34b21da861b663d059a7589e
20180032 <54964850+20180032@users.noreply.github.com>
2019-11-07 18:44:10 -0400

    Los puntos sustraidos estan en la hoja de excel. Sebastian Sanchez 2018-0032

--- 10135714/portafolio1/punto3/punto3.c
+++ 10135714/portafolio1/punto3/punto3.c
@@ -4,7 +4,7 @@
 
 #include "punto3.h"
 
-int imprimir_primos(int n1, int n2){
+int imprimir_primos(int n1, int n2){           //El codigo funcionaba pero el ejercicio pedia los numeros primos dentro de un rango de numeros por ello hay que penalizarlo 85/100
 
     int cont, i, primo;
     for ( i = n1 ; i <= n2 ; i++ )
--- 10135714/portafolio1/punto4/main.c
+++ 10135714/portafolio1/punto4/main.c
@@ -1,6 +1,6 @@
 #include "punto4.h"
 
 int main() {
-    printf("Escriba una frase y luego presione espacio y enter: \n"); //No deberia de darle espacio para que el programa funcione
+    printf("Escriba una frase y luego presione espacio y enter: \n"); //No deberia de darle espacio para que el programa funcione pero en si el codigo cumple con el ejercicio 95/100. Sebastian Sanchez
     inv();
 }
