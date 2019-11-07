4641bbb6337b72d1f6f24019846852f9d935fd0b
nicoluv <55165208+nicoluv@users.noreply.github.com>
2019-11-07 12:04:43 -0400

    -nicol ureña

--- 10123226/Portafolio_1/Problema_5.c
+++ 10123226/Portafolio_1/Problema_5.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>// -5
 
 //Prototipo de las funciones
 void inversa(int[],int);
-//Variables globales
-int i,j;
+//Variables globales//-10 CUIDADO CON LAS VARIABLES GLOBALES INNECESARIAS
+
 
 int main()
 {
-  int cantidad;
-  printf("Por favor indique la cantidad de numeros que usted desea digitar: ");//se especifica la cantidad de
-  scanf("%d",&cantidad);                                                       //numeros para saber el tamano
-                                                                               //del arreglo.
-  int numeros[cantidad];
+    int cantidad;
+    int i;
+    printf("Por favor indique la cantidad de numeros del arreglo: ");//se especifica la cantidad de
+    scanf("%d",&cantidad);                                                       //numeros para saber el tamano
+    //del arreglo.
+    int numeros[cantidad];
     for(i=0;i<cantidad;i++)
     {
         printf("Por favor digite el valor numero %d: ",i+1);//for para almacenar los valores
@@ -28,14 +29,15 @@ int main()
 //funciones desarrolladas
 void inversa(int numeros[],int cantidad)
 {
-   int neew_arreglo[cantidad];
+    int neew_arreglo[cantidad];
+    int i,j;
 
     for(i=cantidad-1,j=0;i>=0;i--,j++)//for en el que 'i' sera el orden del nuevo arreglo
     {                                 //y 'j' el del arreglo de los numeros. los valores del
         neew_arreglo[i] = numeros[j]; //viejo arreglo se van colocando en la ultima posicion del nuevo.
     }
 
-    for(i=0;i<cantidad;i++)//for el cual imprime el nuevo arreglo ya con los valores asignados.
+    for(i=0;i < cantidad; i++)//for el cual imprime el nuevo arreglo ya con los valores asignados.
     {
         printf("%d ",neew_arreglo[i]);
     }
